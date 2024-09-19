4883a29cdbd3918ac5dbf74fb575095f655c9cfa
mugilan602 <mugilan7778@gmail.com>
2024-09-19 18:12:52 +0530

    changed main.jsx file location

--- main.jsx
+++ main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Pages/App";
+import App from "./src/Pages/App";
 import { BrowserRouter } from "react-router-dom";
-import Footer from "./Pages/Footer/Footer";
-import { AuthContextProvider } from "./context/AuthContext";
-import { AdminAuthContextProvider } from "./context/AdminAuthContext";
+import Footer from "./src/Pages/Footer/Footer";
+import { AuthContextProvider } from "./src/context/AuthContext";
+import { AdminAuthContextProvider } from "./src/context/AdminAuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
