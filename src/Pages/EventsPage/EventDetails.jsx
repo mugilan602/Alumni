31fbd3b263d02f9206e92c5306ae051dcd69739f
Mugilan <mugilan7778@gmail.com>
2024-09-26 10:05:20 +0530

    Changed the text

--- src/Pages/EventsPage/EventDetails.jsx
+++ src/Pages/EventsPage/EventDetails.jsx
@@ -24,47 +24,49 @@ const EventDetails = () => {
 
     return (
         <>
-            <Navbar2 />
-            <Campus />
-            <div className="container mx-auto p-6">
-                {/* Event Name Heading */}
-                <h1 className="text-3xl sm:text-4xl font-bold mb-4 text-center underline text-[#005D99] p-4 rounded-md">
-                    {event.name}
-                </h1>
+            <div className='bg-white'>
+                <Navbar2 />
+                <Campus />
+                <div className="container mx-auto p-6">
+                    {/* Event Name Heading */}
+                    <h1 className="text-3xl sm:text-4xl font-bold mb-4 text-center underline text-[#005D99] p-4 rounded-md">
+                        {event.name}
+                    </h1>
 
-                {/* Carousel Section */}
-                <div className="relative w-full max-w-xl mx-auto">
-                    <Carousel
-                        autoPlay
-                        interval={3000}
-                        infiniteLoop
-                        showThumbs={false}
-                        showStatus={false}
-                        showIndicators={true}
-                        dynamicHeight={false}
-                        showArrows={false}
-                        stopOnHover={false}
-                    >
-                        {event.images.map((image, index) => (
-                            <div key={index}>
-                                <img
-                                    src={image}
-                                    alt={`Event image ${index + 1}`}
-                                    className="rounded-lg object-contain"
-                                    style={{ height: '450px', width: '100%' }}
-                                />
-                            </div>
-                        ))}
-                    </Carousel>
-                </div>
+                    {/* Carousel Section */}
+                    <div className="relative w-full max-w-xl mx-auto">
+                        <Carousel
+                            autoPlay
+                            interval={3000}
+                            infiniteLoop
+                            showThumbs={false}
+                            showStatus={false}
+                            showIndicators={true}
+                            dynamicHeight={false}
+                            showArrows={false}
+                            stopOnHover={false}
+                        >
+                            {event.images.map((image, index) => (
+                                <div key={index}>
+                                    <img
+                                        src={image}
+                                        alt={`Event image ${index + 1}`}
+                                        className="rounded-lg object-contain"
+                                        style={{ height: '450px', width: '100%' }}
+                                    />
+                                </div>
+                            ))}
+                        </Carousel>
+                    </div>
 
-                {/* Description Heading */}
-                <div className='flex justify-center'>
-                    <div className="mt-6  text-center w-[90%] sm:text-left">
-                        <h2 className="text-2xl sm:text-3xl font-semibold mb-4  text-[#005D99]  rounded-md">
-                            Description
-                        </h2>
-                        <p className="text-lg  text-justify leading-relaxed">{event.description}</p>
+                    {/* Description Heading */}
+                    <div className='flex justify-center'>
+                        <div className="mt-6  text-center w-[90%] sm:text-left">
+                            <h2 className="text-2xl sm:text-3xl font-semibold mb-4  text-[#005D99]  rounded-md">
+                                Description
+                            </h2>
+                            <p className="text-lg text-black  text-justify leading-relaxed">{event.description}</p>
+                        </div>
                     </div>
                 </div>
             </div>
